f1b49d2113090630c5f787fa943a6ab832eaa008
SongzzZ <9771981687@qq.com>
2019-05-20 19:09:10 +0800

    change gradient

--- BPR.py
+++ BPR.py
@@ -5,7 +5,7 @@
 @Author: ZhaoSong
 @LastEditors: ZhaoSong
 @Date: 2019-04-10 13:38:23
-@LastEditTime: 2019-04-14 16:50:56
+@LastEditTime: 2019-05-19 17:35:48
 '''
 import pandas as pd 
 import numpy as np
@@ -83,8 +83,8 @@ def bpr_mf(user_count, item_count, hidden_dim):
     
     # AUC for one user:
     # reasonable if all (u,i,j) pairs are from the same user
-    # average AUC = mean( auc for each user in test set)
-    mf_auc = tf.reduce_mean(tf.to_float(x>0))
+    # average AUC = mean( accuracy for each user in test set)
+    mf_accuracy = tf.reduce_mean(tf.to_float(x>0))
 
     l2_norm = tf.add_n([
         tf.reduce_sum(tf.multiply(u_emb, u_emb)),
@@ -95,7 +95,7 @@ def bpr_mf(user_count, item_count, hidden_dim):
     bprloss = regulation_rate * l2_norm - tf.reduce_mean(tf.log(tf.sigmoid(x)))
     train_op = tf.train.GradientDescentOptimizer(0.01).minimize(bprloss)
 
-    return u,i,j,mf_auc,bprloss,train_op    
+    return u,i,j,mf_accuracy,bprloss,train_op    
 
 # ListRank MF
 def LTR_MF(records, ratings, user_ratings, item_count):
@@ -106,13 +106,14 @@ with tf.Session() as sess:
     user_count, item_count, user_ratings = data_processor()
     user_ratings_test = get_test(user_count,user_ratings)
 
-    u, i, j, mf_auc, bprloss, train_op = bpr_mf(user_count, item_count, 20)
+    dim = 100
+    u, i, j, mf_accuracy, bprloss, train_op = bpr_mf(user_count, item_count, dim)
     
     tf.global_variables_initializer().run()
 
-    for epoch in range(1, 4):
+    for epoch in range(1, 6):
         total_loss = 0
-        for k in range(1, 500): #输入训练三元组
+        for k in range(1, 5000): #输入训练三元组
             uij = get_trainbatch(user_ratings, user_ratings_test, item_count)
             _bprloss, _train_op = sess.run([bprloss, train_op], 
                                 feed_dict={u:uij[:,0], i:uij[:,1], j:uij[:,2]})
@@ -125,16 +126,19 @@ with tf.Session() as sess:
         print ("_train_op")
 
         user_count = 0
-        _auc_sum = 0.0
+        _accuracy_sum = 0.0
 
-        # each batch will return only one user's auc
+        # each batch will return only one user's accuracy
         for t_uij in get_testbatch(user_ratings, user_ratings_test, item_count):
 
-            _auc, _test_bprloss = sess.run([mf_auc, bprloss],
+            _accuracy, _test_bprloss = sess.run([mf_accuracy, bprloss],
                                     feed_dict={u:t_uij[:,0], i:t_uij[:,1], j:t_uij[:,2]})
             user_count += 1
-            _auc_sum += _auc
-        print ("test_loss: ", _test_bprloss, "test_auc: ", _auc_sum/user_count)
+            _accuracy_sum += _accuracy
+        final_accuracy = _accuracy_sum/user_count
+        with open('E:ml/ml-100k/accuracy.txt','a+') as f:
+            f.write('dim=%d\taccuracy=%.4f\n' %(dim,final_accuracy))
+        print ("test_loss: ", _test_bprloss, "test_accuracy: ", final_accuracy)
         print ("")
     variable_names = [v.name for v in tf.trainable_variables()]
     values = sess.run(variable_names)
